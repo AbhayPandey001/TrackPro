296b812ec4bc32f2e3bca3a063629c7a9f6949c0
Abhay Pandey <vasuabhaypandey2005@gmail.com>
2026-04-03 14:50:44 +0530

    add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -10,12 +10,12 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, 'All fields are required to register')
     }
 
-    if ([username, email, password].some(x => x.trim === '')) {
+    if ([username, email, password].some(x => x.trim() === '')) {
         throw new ApiError(400, 'Fields cannot be empty')
     }
 
     // checking if the user already exists
-    const existingUser = User.findOne({
+    const existingUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -23,11 +23,20 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400 , 'User with this email or password already exists')
     }
 
-    const user = User.create({
+    const user = await User.create({
         username ,
         email , 
         password
     })
+
+    const createdUser = await User.findById(user._id).select('-password')
+    if(!createdUser){
+        throw new ApiError(500 , 'User not created ')
+    }
+
+    return res 
+    .status(201)
+    .json(new ApiResponse(201 , createdUser , 'user created successfully'))
 })
 
 export { registerUser }

d4a2a3f17a7e2d64e0347842fda410edba5538b5
Abhay Pandey <vasuabhaypandey2005@gmail.com>
2026-04-11 13:18:56 +0530

    add backend global error handler

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,12 +5,12 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin : process.env.CORS_ORIGIN || 'http://localhost:5173',
-    credentials : true
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    credentials: true
 }))
 
-app.use(express.json({limit : '16kb'}))
-app.use(express.urlencoded({extended : true , limit : '16kb'}))
+app.use(express.json({ limit: '16kb' }))
+app.use(express.urlencoded({ extended: true, limit: '16kb' }))
 app.use(cookieParser())
 
 // importing of routes 
@@ -18,7 +18,11 @@ import userRoutes from './routes/user.routes.js'
 import todoRoutes from './routes/todo.routes.js'
 
 // declaration of routes
-app.use('/api/v1/user' , userRoutes)
-app.use('/api/v1/todo' , todoRoutes)
+app.use('/api/v1/user', userRoutes)
+app.use('/api/v1/todo', todoRoutes)
+
+//error handler {must be at last}
+import errorHandler from './middlewares/error.middleware.js'
+app.use(errorHandler)
 
 export default app 
